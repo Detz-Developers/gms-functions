b9c081d5517ce61b9c928f2f0ddc45024a41034f
Pasindu Randula <pasindusadeep046@gmail.com>
2025-10-03 00:14:08 +0530

    update config.ts

--- functions/src/config.ts
+++ functions/src/config.ts
@@ -1,4 +1,6 @@
+// src/config.ts
 import { setGlobalOptions } from "firebase-functions/v2";
+import "dotenv/config";
 
 export const REGION = "us-central1";
 export const DEFAULT_CPU = 0.25;
@@ -7,4 +9,3 @@ setGlobalOptions({
   region: REGION,
   cpu: DEFAULT_CPU
 });
-
